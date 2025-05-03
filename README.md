e173a46aebbb66b89bc986e9802ecd622e852109
bars1k <gnoevoyandrey044@gmail.com>
2025-05-03 11:29:55 +0200

    properly worked script with readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple script to pick up randomly new words / phrases from csv file, as well as, already passed values for learning.
+
+**Data sources:**
+- Oxford 3000 word list: https://github.com/sapbmw/The-Oxford-3000/blob/master/The_Oxford_3000.txt
+- Phrases list: https://github.com/jnoodle/English-Vocabulary-Word-List/blob/master/Oxford%20Phrase%20List.txt
+
--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import numpy as np
 import random
 
 
-def get_values(df, num1, num2):
+def get_values(df, col, n1, n2):
     # pick up new random values
     new_values = df.query("is_passed == False")
-    new_values_lst = random.choices(new_values["value"].to_list(), k=num1)
+    new_values_lst = random.choices(new_values[col].to_list(), k=n1)
 
     # update table
-    df["is_passed"] = np.where(df["value"].isin(new_values_lst), True, df["is_passed"])
+    df["is_passed"] = np.where(df[col].isin(new_values_lst), True, df["is_passed"])
 
     # select passed values
     passed_values = df.query("is_passed == True")
-    passed_values_lst = random.choices(passed_values["value"].to_list(), k=num2)
+    passed_values_lst = random.choices(passed_values[col].to_list(), k=n2)
 
     return new_values_lst, passed_values_lst
 
 
 def main():
-    words = pd.read_csv("words.csv")
-    phrases = pd.read_csv("phrases.csv")
+    words = pd.read_csv("data/words.csv")
+    phrases = pd.read_csv("data/phrases.csv")
 
-    new_words, passed_words = get_values(words, 20, 10)
-    new_phrases, passed_phrases = get_values(phrases, 3, 1)
+    new_words, passed_words = get_values(words, "word", 20, 10)
+    new_phrases, passed_phrases = get_values(phrases, "phrase", 3, 1)
+
+    # save changes to the CSV files
+    words.to_csv("data/words.csv", index=False)
+    phrases.to_csv("data/phrases.csv", index=False)
 
     print("New words:", new_words)
     print("Passed words:", passed_words)
